b436171433fb6b3f200d357552145e00e5ad7d22
jossyboydgenius <jossyboydgenius@gmail.com>
2024-01-24 15:35:07 +0000

    Initial commit

--- 010_classes_and_imports/main.dart
+++ 010_classes_and_imports/main.dart
@@ -1,1 +1,8 @@
-void main(List<String> args) {}
+import 'ball.dart';
+void main(List<String> args) {
+  Ball ball1 = Ball('ball1');
+  Ball ball2 = Ball('ball2');
+  ball1.inflate();
+  ball1.bounce();
+  ball2.roll();
+}
