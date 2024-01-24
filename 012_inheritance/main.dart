a327bc2bea8943c8e9481767071bc13241809a3f
jossyboydgenius <jossyboydgenius@gmail.com>
2024-01-24 18:48:27 +0000

    Initial commit

--- 012_inheritance/main.dart
+++ 012_inheritance/main.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+import 'basketball.dart';
+
+void main(List<String> args) {
+  Basketball basketball = Basketball('basketball');
+  basketball.bounce();
+  basketball.Throwball();
+}
--- 013_mixins/main.dart
+++ 013_mixins/main.dart
@@ -1,1 +1,8 @@
-void main(List<String> args) {}
+import 'Volleyball.dart';
+
+void main(List<String> args) {
+  Volleyball volleyball = Volleyball('volleyball');
+  volleyball.bounce();
+  volleyball.Throwball('volleyball');
+  volleyball.caught('volleyball');
+}
